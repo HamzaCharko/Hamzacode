ba1af403db20b310781dd6b645be7a8a08b2de87
HamzaCharko <48889435+HamzaCharko@users.noreply.github.com>
2019-03-24 19:42:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,19 @@
+
 const Discord = require('discord.js');
 cosnst client = new Discord.Client();
 const prefix = "!"
- 
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setActivity('', { type: 'PLAYING' })
 });
- 
- 
- 
- 
- 
- 
- 
- 
- 
-client.login('NTQ5Njc3MjU2Njg0MDc3MDU2.D3lcwA.zoL7eVAiMU7N4zqgA4zfY8dNncc');
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
